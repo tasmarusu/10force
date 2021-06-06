209f07c9f737e0b7ef6f1d8d23b7145fa67d5f5f
山村豊 <y-yamamura@gmail.com>
2021-06-06 23:38:38 +0900

    MainScene.csの初期化からボタン押して開始まで。 PlayerInput.csの作成

--- Assets/Game/02Script/MainScene.cs
+++ Assets/Game/02Script/MainScene.cs
@@ -1,12 +1,74 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿/* *************************************************
+* InGameの始まりから終わりまでを監視する
+************************************************* */
+
+
 
 namespace MainForce
 {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UniRx;
+
     public class MainScene : MonoBehaviour
     {
-        
+        [SerializeField] private PlayerInput playerInput = null;
+
+
+
+        private CompositeDisposable disposables = new CompositeDisposable();
+
+
+        private void Start()
+        {
+            this.Init();
+        }
+
+
+        /// <summary>
+        /// 全体の初期化
+        /// </summary>
+        private void Init()
+        {
+            this.playerInput.Init();
+
+
+            this.SelectChara();
+        }
+
+
+        /// <summary>
+        /// InGame で使う 自機 を決める
+        /// </summary>
+        private void SelectChara()
+        {
+            Debug.Log($"SelectChara {Time.frameCount}");
+
+            Observable.EveryUpdate().Subscribe(_ =>
+            {
+                // 自機決定したら次へ
+                if (this.playerInput.IsClick() == true)
+                {
+                    this.disposables.Clear();
+                    this.InGamePlay();
+                }
+            }).AddTo(disposables);
+
+
+        }
+
+
+        /// <summary>
+        /// InGame で使う関数などの呼び出し
+        /// </summary>
+        private void InGamePlay()
+        {
+            Observable.EveryUpdate().Subscribe(_ =>
+            {
+                Debug.Log($"frameCount {Time.frameCount}");
+            }).AddTo(disposables);
+        }
     }
 }
 
---
+++ Assets/Game/02Script/PlayerInput.cs
@@ -0,0 +1,33 @@
+﻿/* *************************************************
+* InGameの始まりから終わりまでを監視する
+************************************************* */
+
+
+
+namespace MainForce
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UniRx;
+
+    public class PlayerInput : MonoBehaviour
+    {
+        /// <summary>
+        /// Input系の初期化
+        /// </summary>
+        public void Init()
+        {
+            Debug.Log($"{this} の初期化");
+        }
+
+        /// <summary>
+        /// クリックテスト
+        /// </summary>
+        /// <returns></returns>
+        public bool IsClick()
+        {
+            return Input.GetKey(KeyCode.S);
+        }
+    }
+}
