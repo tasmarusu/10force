db5052363ebfd84d9cc95e24a9fb6e1201b692a8
山村豊 <y-yamamura@gmail.com>
2021-06-08 23:39:49 +0900

    MainSceneUIの作成

---
+++ Assets/Game/02Scripts/Scene/StartInitialize.cs
@@ -0,0 +1,17 @@
+/* *************************************************
+* MainSceneUI InGame ‚Ì UI•\Ž¦
+************************************************* */
+namespace MainForce
+{
+	using UnityEngine;
+
+	public class StartInitialize : MonoBehaviour
+	{
+
+		[RuntimeInitializeOnLoadMethod()]
+		static void Init()
+		{
+			
+		}
+	}
+}
---
+++ Assets/Game/02Scripts/UI/MainSceneUI.cs
@@ -0,0 +1,20 @@
+/* *************************************************
+* MainSceneUI InGame ‚Ì UI•\Ž¦
+************************************************* */
+namespace MainForce
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class MainSceneUI : MonoBehaviour
+    {
+        [SerializeField] private PlayerInput playerInput = null;
+
+
+        public void Init()
+        {
+
+        }
+    }
+}
