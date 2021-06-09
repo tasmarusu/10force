05d1bba1991ae8d01424a6f40bf10df3e104a9c9
山村豊 <y-yamamura@gmail.com>
2021-06-10 00:24:47 +0900

    UIシーンを呼ぼうと思ったが失敗、取り合えず記述した内容はあってる

--- Assets/Game/02Scripts/Scene/StartInitialize.cs
+++ Assets/Game/02Scripts/Scene/StartInitialize.cs
@@ -4,14 +4,15 @@
 namespace MainForce
 {
 	using UnityEngine;
+	using UnityEngine.SceneManagement;
 
 	public class StartInitialize : MonoBehaviour
 	{
-
 		[RuntimeInitializeOnLoadMethod()]
 		static void Init()
 		{
-			
+			SceneManager.LoadScene("MainSceneUI", LoadSceneMode.Additive);
+			SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
 		}
 	}
 }
